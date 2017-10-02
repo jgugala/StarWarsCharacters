efdcb0185250ffa57421ad007e29b716b075729c
Jakub Gugała <jgugala@pgs-soft.com>
2017-10-02 13:24:26 +0200

    Initial commit

---
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/view/StarWarsCharactersActivity.java
@@ -0,0 +1,15 @@
+package com.hpk.solutions.starwarscharacters.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hpk.solutions.starwarscharacters.R;
+
+public class StarWarsCharactersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_wars_characters);
+    }
+}
