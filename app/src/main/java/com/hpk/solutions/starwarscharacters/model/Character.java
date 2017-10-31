a7df01bd05f9c1b0ee56417f7887a73691176e0c
Jakub Gugała <jgugala@pgs-soft.com>
2017-10-31 12:24:29 +0100

    Character added to the data model.

---
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/model/Character.java
@@ -0,0 +1,78 @@
+package com.hpk.solutions.starwarscharacters.model;
+
+/**
+ * Created by jgugala on 2017-10-31.
+ */
+
+public class Character {
+
+    private String name;
+
+    private int height;
+
+    private int mass;
+
+    private String hairColor;
+
+    private String eyeColor;
+
+    private String birthYear;
+
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
