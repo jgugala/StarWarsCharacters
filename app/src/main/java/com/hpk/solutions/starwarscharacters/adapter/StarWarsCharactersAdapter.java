5b8cbda4158552903cccd14ca910f4bcc4c12418
Jakub Gugała <jgugala@pgs-soft.com>
2018-01-08 11:27:15 +0100

    SDK version increased

---
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/adapter/StarWarsCharactersAdapter.java
@@ -0,0 +1,49 @@
+package com.hpk.solutions.starwarscharacters.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hpk.solutions.starwarscharacters.model.Character;
+
+import java.util.List;
+
+/**
+ * Created by jgugala on 2017-10-31.
+ */
+
+public class StarWarsCharactersAdapter extends RecyclerView.Adapter<StarWarsCharactersAdapter.ViewHolder> {
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, Character item, int position);
+
+    }
+
+    private List<Character> characters;
+
+    private OnItemClickListener onItemClickListener;
+
+    public StarWarsCharactersAdapter(List<Character> characters){}
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
