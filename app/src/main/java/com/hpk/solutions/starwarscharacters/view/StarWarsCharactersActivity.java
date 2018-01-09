cac587bb250cb782e6b335a20dc41c9ac28de3ec
Jakub Gugała <jgugala@pgs-soft.com>
2018-01-09 13:03:51 +0100

    Adapter initialized in the activity and attached to the list.

--- app/src/main/java/com/hpk/solutions/starwarscharacters/view/StarWarsCharactersActivity.java
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/view/StarWarsCharactersActivity.java
@@ -3,20 +3,26 @@ package com.hpk.solutions.starwarscharacters.view;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
 import com.hpk.solutions.starwarscharacters.R;
 import com.hpk.solutions.starwarscharacters.adapter.StarWarsCharactersAdapter;
+import com.hpk.solutions.starwarscharacters.databinding.ActivityStarWarsCharactersBinding;
+import com.hpk.solutions.starwarscharacters.model.Character;
 
-public class StarWarsCharactersActivity extends AppCompatActivity {
+public class StarWarsCharactersActivity extends AppCompatActivity implements StarWarsCharactersAdapter.OnItemClickListener {
 
     private StarWarsCharactersAdapter adapter;
 
     private StarWarsCharactersViewModel viewModel;
 
+    private ActivityStarWarsCharactersBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.activity_star_wars_characters);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_star_wars_characters);
         viewModel = new StarWarsCharactersViewModel();
     }
 
@@ -28,5 +34,13 @@ public class StarWarsCharactersActivity extends AppCompatActivity {
 
     private void initRecyclerView() {
         adapter = new StarWarsCharactersAdapter(viewModel.getCharacters());
+        adapter.setOnItemClickListener(this);
+        binding.starWarsCharactersList.setLayoutManager(new LinearLayoutManager(this));
+        binding.starWarsCharactersList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, Character item, int position) {
+
     }
 }
