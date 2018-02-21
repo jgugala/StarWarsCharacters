89973fc6fee4698ffc53c6b860cc2784cdbb9cae
Jakub Gugała <jgugala@pgs-soft.com>
2018-02-21 13:10:02 +0100

    Application class created and registered in the manifest file.

---
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/StarWarsCharactersApp.java
@@ -0,0 +1,10 @@
+package com.hpk.solutions.starwarscharacters;
+
+import android.app.Application;
+
+/**
+ * Created by jgugala on 2018-02-20.
+ */
+
+public class StarWarsCharactersApp extends Application {
+}
