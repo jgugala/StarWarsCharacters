02e70ddfd955f74b0e9b43dc9a5f454bc9048644
Jakub Gugała <jgugala@pgs-soft.com>
2018-02-21 14:13:14 +0100

    ActivityModule updated with 'ContributesAndroidInjector' annotation. Unnecessary provider removed.

--- app/src/main/java/com/hpk/solutions/starwarscharacters/di/modules/ActivityModule.java
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/di/modules/ActivityModule.java
@@ -1,22 +1,21 @@
 package com.hpk.solutions.starwarscharacters.di.modules;
 
+import com.hpk.solutions.starwarscharacters.view.StarWarsCharactersActivity;
 import com.hpk.solutions.starwarscharacters.view.StarWarsCharactersViewModel;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
 
 /**
  * Created by jgugala on 2018-01-29.
  */
 
 @Module
-public class ActivityModule {
+public abstract class ActivityModule {
 
-    @Provides
-    @Singleton
-    StarWarsCharactersViewModel provideStarWarsCharactersViewModel() {
-        return new StarWarsCharactersViewModel();
-    }
+    @ContributesAndroidInjector
+    abstract StarWarsCharactersActivity contributeStarWarsCharactersActivity();
 }
