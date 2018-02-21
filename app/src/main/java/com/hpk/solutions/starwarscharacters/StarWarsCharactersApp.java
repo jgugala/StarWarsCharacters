8e1dedb19ed76ca715a92c1e379ec537a83be85a
Jakub Gugała <jgugala@pgs-soft.com>
2018-02-21 14:14:09 +0100

    Application class extended by the dagger interface implementation.

--- app/src/main/java/com/hpk/solutions/starwarscharacters/StarWarsCharactersApp.java
+++ app/src/main/java/com/hpk/solutions/starwarscharacters/StarWarsCharactersApp.java
@@ -1,10 +1,34 @@
 package com.hpk.solutions.starwarscharacters;
 
+import android.app.Activity;
 import android.app.Application;
+
+import com.hpk.solutions.starwarscharacters.di.components.DaggerAppComponent;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
 
 /**
  * Created by jgugala on 2018-02-20.
  */
 
-public class StarWarsCharactersApp extends Application {
+public class StarWarsCharactersApp extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DaggerAppComponent.builder().application(this).build().inject(this);
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
 }
